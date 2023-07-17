41fc8321be2dd8b5b0b86682d304af928c97a4e0
anujrmohite <anujmohite001@gmail.com>
2023-07-17 09:28:41 +0530

    psuh

---
+++ localusers/luser-demo01.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USER_ID=${UID}
+echo "The user id is: ${USER_ID}"
+
+
+
+
+
+
+
+# # Display the UID and username of the user executing this script
+# # Display if the user is the root user or not
+
+# # Display the UID
+
+# echo "Your UID id ${UID}"
+
+# # Display the username
+
+# USER_NAME=$(id -un)
+# echo "Your username is:${USER_NAME}"
+
+# # Display if the user is the root user or not
+# if [[ "${UID}" -eq 0 ]]; then
+#     echo 'You are root'
+# else
+#     echo 'You are not root'
+# fi
+
+
